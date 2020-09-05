16254979d27e60435b5c78d8691a259c1535befd
Eagle's <surendrakrishna.sk@gmail.com>
2020-09-05 16:21:42 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+var app = angular.module("contactsApp", []);
+
+app.controller('mainCtrl', function($scope, $http){
+	$scope.contacts={};
+	$http({
+  method: 'GET',
+  url: 'https://run.mocky.io/v3/756d0e19-daee-4acd-9c9e-84fb108e2b04'
+}).then(function successCallback(response) {
+	console.log(response);
+	$scope.contacts = response.data;
+    // this callback will be called asynchronously
+    // when the response is available
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+})

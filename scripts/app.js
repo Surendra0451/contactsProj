5e04b37cf15a2745fc6432c21c17d3b0afefaf0d
Eagle's <surendrakrishna.sk@gmail.com>
2020-09-05 20:24:47 +0530

    contacts rendered

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,24 @@ var app = angular.module("contactsApp", []);
 
 app.controller('mainCtrl', function($scope, $http){
 	$scope.contacts={};
+	
+	$scope.editRecord = function(id){
+		
+	}
+	$scope.deleteRecord = function(id){
+		
+	}
 	$http({
-  method: 'GET',
-  url: 'https://run.mocky.io/v3/756d0e19-daee-4acd-9c9e-84fb108e2b04'
-}).then(function successCallback(response) {
-	console.log(response);
-	$scope.contacts = response.data;
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+		method: 'GET',
+		url: 'https://run.mocky.io/v3/756d0e19-daee-4acd-9c9e-84fb108e2b04'
+	}).then(function successCallback(response) {
+		console.log(response);
+		$scope.contacts = response.data;
+		
+		// this callback will be called asynchronously
+		// when the response is available
+	}, function errorCallback(response) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+	});
 })
